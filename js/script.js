e322ad8fabd0867c78da96773533638b4c07b205
Brandon1994 <brandon2_@hotmail.com>
2020-07-26 20:12:03 -0300

    Implementing some features to control the screen

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 let allPeople = [];
 
-window.addEventListener("load", () => {
-  document.getElementById("search").addEventListener("click", handleClick);
+window.addEventListener('load', () => {
+  document.getElementById('search').addEventListener('click', handleClick);
+  document
+    .getElementById('nameToSearch')
+    .addEventListener('keyup', handleInputType);
   addLoading();
   getData().then((data) => {
     allPeople = transform(data);
@@ -27,28 +30,37 @@ function transform(data) {
 
 async function getData() {
   const res = await fetch(
-    "https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo"
+    'https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo'
   );
   return await res.json();
 }
 
 function removeLoading() {
-  var elem = document.querySelector(".progress");
+  var elem = document.querySelector('.progress');
   elem.parentNode.removeChild(elem);
 }
 
 function addLoading() {
-  let loadingDiv = document.getElementById("loading");
-  let progressDiv = document.createElement("div");
-  let childDiv = document.createElement("div");
-  progressDiv.classList.add("progress");
-  childDiv.classList.add("indeterminate");
+  let loadingDiv = document.getElementById('loading');
+  let progressDiv = document.createElement('div');
+  let childDiv = document.createElement('div');
+  progressDiv.classList.add('progress');
+  childDiv.classList.add('indeterminate');
   progressDiv.appendChild(childDiv);
   loadingDiv.appendChild(progressDiv);
 }
 
+function handleInputType() {
+  if (document.getElementById('nameToSearch').value === '')
+    document.getElementById('search').classList.value =
+      'waves-effect waves-light btn-small disabled';
+  else
+    document.getElementById('search').classList.value =
+      'waves-effect waves-light btn-small';
+}
+
 function handleClick() {
-  let value = document.getElementById("nameToSearch").value;
+  let value = document.getElementById('nameToSearch').value;
   search(value);
 }
 
@@ -56,7 +68,10 @@ function search(valueToSearch) {
   let filteredPeople = allPeople.filter((person) => {
     return person.name.toLowerCase().includes(valueToSearch.toLowerCase(), 0);
   });
-  if (filteredPeople.length === 0) return;
+  if (filteredPeople.length === 0) {
+    clearResults();
+    return;
+  }
   let totalMale = calculateMaleGender(filteredPeople);
   let totalFemale = calculateFemaleGender(filteredPeople);
   let ageSum = calculateAgeSum(filteredPeople);
@@ -65,9 +80,30 @@ function search(valueToSearch) {
   fillHtmlWithResult(filteredPeople);
 }
 
+function clearResults() {
+  let peopleList = document.getElementById('people');
+  peopleList.innerHTML = `
+    <li class="collection-item">
+      <h3 class="total-users">
+        Nenhum usuário encontrado
+      </h3>
+    </li>
+  `;
+
+  let statiscsList = document.getElementById('statiscsList');
+  statiscsList.innerHTML = `
+    <li class="collection-item">
+      <h3 class="statistics">
+        Nenhum usuário encontrado
+      </h3>
+    </li>
+  `;
+
+}
+
 function calculateMaleGender(result) {
   return result.reduce((accumulator, current) => {
-    if (current.gender === "male") {
+    if (current.gender === 'male') {
       accumulator++;
     }
     return accumulator;
@@ -76,7 +112,7 @@ function calculateMaleGender(result) {
 
 function calculateFemaleGender(result) {
   return result.reduce((accumulator, current) => {
-    if (current.gender === "female") {
+    if (current.gender === 'female') {
       accumulator++;
     }
     return accumulator;
@@ -90,38 +126,34 @@ function calculateAgeSum(result) {
 }
 
 function fillHtmlWithResult(result) {
-  let totalUsers = document.querySelector(".total-users");
-  totalUsers.textContent = `${result.length} usuário(s) encontrado(s)`;
-  let statistics = document.querySelector(".statistics");
-  statistics.textContent = "Estatísticas";
-  result.forEach((element) => {
-    let list = document.getElementById("people");
-    let liElement = document.createElement("li");
-    let userImg = document.createElement("img");
-    userImg.src = element.picture;
-    liElement.textContent = `${element.name}, ${element.age} anos`;
-    liElement.classList.add("collection-item");
-    liElement.appendChild(userImg);
-    list.appendChild(liElement);
-  });
+  let list = document.getElementById('people');
+  list.innerHTML = `
+  <li class="collection-item">
+    <h3 class="total-users">${result.length} usuário(s) encontrado(s)</h3>
+  </li>
+  ${result
+    .map((element) => {
+      return `
+    <li class='collection-item'>
+      <img src='${element.picture}'>
+      </img>
+      ${element.name}, ${element.age} anos
+    </li>
+    `;
+    })
+    .join('')}
+    `;
 }
 
-function fillHtmlStatistics(totalMale, totalFemale, ageSum, ageRate){
+function fillHtmlStatistics(totalMale, totalFemale, ageSum, ageRate) {
   let statisctisList = document.getElementById('statiscsList');
-  let liTotalMale = document.createElement("li");
-  liTotalMale.textContent = `Sexo masculino: ${totalMale}`;
-
-  let liTotalFemale = document.createElement("li");
-  liTotalFemale.textContent = `Sexo feminino: ${totalFemale}`;
-
-  let liAgeSum = document.createElement("li");
-  liAgeSum.textContent = `Soma das idades: ${ageSum}`;
-
-  let liAgeRate = document.createElement("li");
-  liAgeRate.textContent = `Média das idades: ${ageSum}`;
-
-  statisctisList.appendChild(liTotalMale);
-  statisctisList.appendChild(liTotalFemale);
-  statisctisList.appendChild(liAgeSum);
-  statisctisList.appendChild(liAgeRate);
+  statisctisList.innerHTML = `
+    <li class="collection-item ">
+      <h3 class="statistics">Estatísticas</h3>
+    </li>
+    <li class="collection-item ">Sexo masculino: ${totalMale}</li>
+    <li class="collection-item ">Sexo feminino: ${totalFemale}</li>
+    <li class="collection-item ">Soma das idades: ${ageSum}</li>
+    <li class="collection-item ">Média das idades: ${ageRate}</li>
+  `;
 }
